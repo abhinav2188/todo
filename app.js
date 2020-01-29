96ce24a72b567db105800fe60b95d4e8f3db9d23
abhinav2188 <abhinav.mca19.du@gmail.com>
2020-01-29 23:09:49 +0530

    added mongodb atlas

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const _ = require('lodash');
 
-mongoose.connect('mongodb://localhost:27017/todo',{ useUnifiedTopology: true , useNewUrlParser: true });
+mongoose.connect('mongodb+srv://abhi:1234@cluster0-tbra7.mongodb.net/todoListDB',{ useUnifiedTopology: true , useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -12,9 +12,6 @@ app.use(express.static('public/'));
 
 app.set('view engine', 'ejs');
 
-app.listen(3000,function(){
-  console.log("server started at port 3000");
-});
 
 //schemas and models
 const itemSchema = new mongoose.Schema({
@@ -29,11 +26,9 @@ const listSchema = new mongoose.Schema({
   items : [itemSchema]
 });
 const List = mongoose.model('list',listSchema);
-let defaultItems = [
-  {
+let defaultItem=  new Item({
     name:'default item'
-  }
-];
+  });
 
 var date = new Date().toLocaleString('en-US' , {
   weekday : 'long',
@@ -54,7 +49,7 @@ app.get('/',function(req,res){
 // adding item in a list
 app.post('/add-item',(req,res) => {
   let newItem = req.body.newItem;
-  let listTitle = _.lowerCase(req.body.list);
+  let listTitle = req.body.list;
   let item = new Item({
     name : req.body.newItem
   });
@@ -62,7 +57,7 @@ app.post('/add-item',(req,res) => {
     item.save();
     res.redirect('/');
   }else{
-    List.findOne({name:listTitle},function(err,foundList){
+    List.findOne({name:_.lowerCase(listTitle)},function(err,foundList){
       if(!err){
         foundList.items.push(item);
         foundList.save();
@@ -75,7 +70,7 @@ app.post('/add-item',(req,res) => {
 // deleting item from list
 app.post('/delete-item/:id' , (req,res) => {
   let id = req.params.id;
-  let listTitle = _.lowerCase(req.body.list);
+  let listTitle = req.body.list;
   if(listTitle === date){
     Item.deleteOne({_id : id} ,function(err){
       if (err)
@@ -83,7 +78,7 @@ app.post('/delete-item/:id' , (req,res) => {
     });
     res.redirect('/');
   }else{
-    List.updateOne({name:listTitle},{$pull:{items:{_id:id}}}, function(err){
+    List.updateOne({name:_.lowerCase(listTitle)},{$pull:{items:{_id:id}}}, function(err){
       if(err)
       console.log("error in deleting item");
     });
@@ -102,7 +97,7 @@ app.get('/:customListTitle',(req,res)=>{
       if(!foundList){
         let list = new List({
           name: customListTitle,
-          items : defaultItems
+          items : [defaultItem]
         });
         list.save();
         res.redirect('/'+customListTitle);
@@ -116,4 +111,13 @@ app.get('/:customListTitle',(req,res)=>{
     }
   });
 
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port,function(){
+  console.log("server started at port "+port);
 });
